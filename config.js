6fc4080475117b7d67422d18dbaed799a7045d73
Hjgaming <69000638+Hjgaming@users.noreply.github.com>
2020-12-22 00:48:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.Token = ``; //Bot Token - Important
-exports.Default_Prefix = ``; //Bot Default Prefix - Important
+exports.Token = `NzkwNjU3NTQ2MTM4Mjg4MTU5.X-DzOA.w6quTS_fgpFRJMxam6XnbGqAKuU`; //Bot Token - Important
+exports.Default_Prefix = `$`; //Bot Default Prefix - Important
 exports.Color = ``; //Bot All Embeds Color - Use CAPS For Name - Important
 exports.WelcomeImage = ``; //Welcome Image Link - Default Image By Me (If No Link Provided)
 exports.LeaveImage = ``; //Leave Image Link - Default Image By Me (If No Link Provided)
